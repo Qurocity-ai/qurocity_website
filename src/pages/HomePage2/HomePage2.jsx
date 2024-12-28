1c6f85d593120225b74bc1d4d7836bb256f1b281
ShrawanVD <shra1.deo7714@gmail.com>
2024-12-28 16:44:29 +0530

    added the code

--- src/pages/HomePage2/HomePage2.jsx
+++ src/pages/HomePage2/HomePage2.jsx
@@ -6,10 +6,14 @@ import LearnLanguage from './LearnLanguage'
 import Courselist from '../../components/CourseList/Courselist'
 import WhyWeStandOut from './WhyWeStandOut'
 import styles from "../HomePage2/HomePage2.module.css";
+import LanguageComponent from '../../components/LanguageComponent/LanguageComponent'
+
+
 function HomePage2() {
   return (
     <div className={styles.home_page2}>
       <Navbar2/>
+      <LanguageComponent/>
       <HomeDynamicSection/>
       <Testimonial/>
       <LearnLanguage/>
