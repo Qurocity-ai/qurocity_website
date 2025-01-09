e2da4e404db4df1e0d22daafc576a8e14afa57d9
yogeshmasaistudent <yogeshkushawah93@gmail.com>
2025-01-09 00:27:49 -1200

    small changes

--- src/pages/OneSession/VideoReview.jsx
+++ src/pages/OneSession/VideoReview.jsx
@@ -34,49 +34,49 @@ function VideoReview() {
 
     return (
         <>
-        <div className={VideoStyle.main}>
-            <h3>Meet our language tutor!</h3>
-            <div className={VideoStyle.cardsContainer}>
-                {teachers.length > 0 ? (
-                    teachers.slice(0, visibleCount).map((teacher, index) => ( // Show only visible items
-                        <div key={teacher.id} className={VideoStyle.card}>
-                            <div className={VideoStyle.cardContent}>
-                                <div className={VideoStyle.left}>
-                                    <div className={VideoStyle.imgContainer}>
-                                        <img src={teacher.imgurl} alt="Teacher" />
+            <div className={VideoStyle.main}>
+                <h3>Meet our language tutor!</h3>
+                <div className={VideoStyle.cardsContainer}>
+                    {teachers.length > 0 ? (
+                        teachers.slice(0, visibleCount).map((teacher, index) => ( // Show only visible items
+                            <div key={teacher.id} className={VideoStyle.card}>
+                                <div className={VideoStyle.cardContent}>
+                                    <div className={VideoStyle.left}>
+                                        <div className={VideoStyle.imgContainer}>
+                                            <img src={teacher.imgurl} alt="Teacher" />
+                                        </div>
+                                        <p><span>Language:</span> {teacher.language}</p>
+                                        <p><span>Proficiency level:</span> {teacher.proflevel}</p>
+                                        <p><span>No of students taught till date:</span> {teacher.noStud}</p>
+                                        <p><span>Experience:</span> {teacher.experience}</p>
+                                        <p><span>No of hours taught till date: </span>{teacher.noHours}</p>
                                     </div>
-                                    <p><span>Language:</span> {teacher.language}</p>
-                                    <p><span>Proficiency level:</span> {teacher.proflevel}</p>
-                                    <p><span>No of students taught till date:</span> {teacher.noStud}</p>
-                                    <p><span>Experience:</span> {teacher.experience}</p>
-                                    <p><span>No of hours taught till date: </span>{teacher.noHours}</p>
-                                </div>
-                                <div className={VideoStyle.right}>
-                                    <iframe
-                                        src={teacher.ytvideo}
-                                        title="Teacher Video"
-                                        allowFullScreen
-                                    ></iframe>
+                                    <div className={VideoStyle.right}>
+                                        <iframe
+                                            src={teacher.ytvideo}
+                                            title="Teacher Video"
+                                            allowFullScreen
+                                        ></iframe>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
-                ) : (
-                            <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
-                                <CircularProgress color="secondary" />
-                                <CircularProgress color="success" />
-                                <CircularProgress color="inherit" />
-                            </Stack>
+                        ))
+                    ) : (
+                        <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
+                            <CircularProgress color="secondary" />
+                            <CircularProgress color="success" />
+                            <CircularProgress color="inherit" />
+                        </Stack>
+                    )}
+                </div>
+
+                {/* View More button */}
+                {visibleCount < teachers.length && ( // Show button only if there are more items to display
+                    <button className={VideoStyle.viewMoreButton} onClick={handleViewMore}>
+                        View More
+                    </button>
                 )}
             </div>
-
-            {/* View More button */}
-            {visibleCount < teachers.length && ( // Show button only if there are more items to display
-                <button className={VideoStyle.viewMoreButton} onClick={handleViewMore}>
-                    View More
-                </button>
-            )}
-        </div>
         </>
     );
 }
